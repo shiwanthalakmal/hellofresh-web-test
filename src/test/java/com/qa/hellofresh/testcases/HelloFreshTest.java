6f9ef52857e55addcc765e40ddb9ab72ad4bc400
Shiwantha Lakmal <Shiwantha.Lakmal@cambio.lk>
2018-11-08 13:05:56 +0530

    added deliberatly failing test case

--- src/test/java/com/qa/hellofresh/testcases/HelloFreshTest.java
+++ src/test/java/com/qa/hellofresh/testcases/HelloFreshTest.java
@@ -53,4 +53,24 @@ public class HelloFreshTest extends BaseTest
                 check_And_Validate_Breadcrumb_Availability().
                 check_And_Validate_Current_URLContains("controller=order-confirmation");
     }
+
+
+
+    /**
+     * Deliberatly failing test case for demonstrate reporting, screen-short and exception categorization
+     * @throws FrameworkException
+     */
+    @Test(groups = {"BAT"})
+    public void site_deliberately_fail_test() throws FrameworkException  {
+        new SiteHomePage(getDriver()).step_Perform_SingIn_Button().
+                step_Create_New_Account_Using_Email().
+                step_Enter_Personal_Information().
+                step_Enter_Address_Information().
+                step_Submit_Account_Details().
+                check_And_Validate_MyAccount_Title("MY ACCOUNT SHIWANTHA !").
+                check_And_Validate_ProfileName().
+                check_And_Validate_Welcome_Message().
+                check_And_Validate_Current_URLContains("controller=my-account").
+                step_Perform_SignOut_Button_If_Available();
+    }
 }
